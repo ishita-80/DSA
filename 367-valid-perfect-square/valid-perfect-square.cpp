ee162763d791ff8cf57490a6fc902c1fe2bb21da
Ishita Agrawal <121822343+ishita-80@users.noreply.github.com>
2025-03-04 22:08:22 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (18.30%) - LeetSync

---
+++ 367-valid-perfect-square/valid-perfect-square.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int s=0,e=num;
+        while(s<=e){
+            long long mid=s+(e-s)/2;
+            if((mid*mid)==num){
+                return true;
+            }
+            else if(mid*mid<num){
+                s=mid+1;
+            }
+            else{
+                e=mid-1;
+            }
+        }
+        return false;
+
+    }
+};
