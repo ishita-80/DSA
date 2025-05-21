81758f389f9eaea45a892f94c319937ecb0f9505
Ishita Agrawal <121822343+ishita-80@users.noreply.github.com>
2025-05-21 15:27:20 +0530

    Time: 0 ms (100%), Space: 37.1 MB (5.69%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int size=nums.size();
+        vector<int> pref(size+1,0);
+        vector<int> suff(size+1,0);
+        for(int i=1,j=size-1;i<size+1 && j>=0 ; i++,j--){
+            pref[i]=pref[i-1]+nums[i-1];
+            suff[j]=suff[j+1]+nums[j];
+        }
+        for(int i=0;i<size;i++){
+            if(pref[i]==suff[i+1]) return i;
+        }
+        return -1;
+
+
+    }
+};
