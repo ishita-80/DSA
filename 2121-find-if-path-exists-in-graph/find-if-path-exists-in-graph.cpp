3ef7dfa0c0a78c14e7422d76164876e0d389177d
Ishita Agrawal <121822343+ishita-80@users.noreply.github.com>
2025-08-26 14:02:37 +0530

    Time: 260 ms (77.24%) | Memory: 281.6 MB (82.67%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.cpp
@@ -1,28 +1,30 @@
 class Solution {
 public:
-    void solve(bool &ans,vector<bool>&visited,vector<vector<int>>& list,int src, int dest){
-        if(src==dest) {
-            ans=true;
-            return;
-        }
-        if(visited[src]==true) return ;
-        visited[src]=true;
-        for(auto child:list[src]){
-            solve(ans,visited,list,child,dest);
-
-        }
-        return;
-    }
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+        if(source==destination) return true;
+        vector<bool>visited(n);
         vector<vector<int>>list(n);
         for(int i=0;i<edges.size();i++){
            
             list[edges[i][0]].push_back(edges[i][1]);
             list[edges[i][1]].push_back(edges[i][0]);
         }
-        vector<bool>visited(n);
-        bool ans=false;
-        solve(ans,visited,list,source,destination);
-        return ans;
+        queue<int>q;
+        q.push(source);
+        visited[source]=true;
+        while(!q.empty()){
+            int front=q.front();
+            q.pop();
+            for(auto it:list[front]){
+                if(!visited[it]){
+                    if(it==destination) return true;
+                    visited[it]=true;
+                    q.push(it);
+                }
+
+            }
+
+        }
+        return false;
     }
 };
